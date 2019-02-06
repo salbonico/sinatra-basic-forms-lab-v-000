59e17cd26d930c232973b9546c1ed1fc71250d85
Steve Albonico <salbonico@gmail.com>
2019-02-06 03:43:29 +0000

    Automatically backed up by Learn

--- spec/sinatra_basic_forms_lab_spec.rb
+++ spec/sinatra_basic_forms_lab_spec.rb
@@ -66,7 +66,7 @@ describe App do
       binding.pry
       expect(page).to have_text(/Name:\s+Butch/i)
       expect(page).to have_text(/Breed:\s+Mastiff/i)
-      expect(page).to have_text(/Age:\s+6/i)
+      expect(page).to have_text(/Age:\s+6 months/i)
 
       #now do it again, to be sure it's not hard-coded
       visit '/new'
